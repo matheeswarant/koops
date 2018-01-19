e8e7f13a87ec97f71e50c84b363e62b38f8b250a
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-01-19 16:52:02 +0530

    Create me.c

---
+++ me.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+ int a, b, sum = 0;
+ printf("Enter a positive integer: ");
+ scanf("%d",&n);
+for(b=1;b <= a;++b)
+ {
+  sum += b;   
+  sum = sum+i;
+ }
+printf("Sum = %d",sum);
+ return 0;
+}
