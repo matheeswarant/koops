0be8cdc403e5753f32bd83533771b5b02765e6ef
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-01-24 16:21:15 +0530

    Create ms.c

---
+++ ms.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100],i;
+printf("/n Enter a string /n");
+scanf("%s",a);
+if(a[i]>='0'&&a[i] <='9')
+{
+printf("yes");
+}
+else 
+{
+printf("no");
+}
+return 0;
+}
