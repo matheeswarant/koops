c126a17a308d37710f026f4551024e82e8d678bf
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-01-24 16:25:39 +0530

    Create mu.c

---
+++ mu.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a[20],i,t;
+printf("/n Enter the size of array /n");
+scanf("%d",&a[i]);
+printf("/n Enter the elements /n");
+scanf("%d",&t);
+for(i=0;i<t;i++);
+{
+printf("%d%d",a[i],i);
+}
+}
