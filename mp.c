4326168ff2e9fa13f95fe0b6d2f6868e29adc00f
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-01-24 16:06:19 +0530

    Create mp.c

---
+++ mp.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int r,i,a[25],min;
+printf("/n enter the numbers /n");
+scanf("%d",&r);
+for(i=0;i<=n;i++)
+{
+min=a[0];
+}
+printf("/n minimum number is%d /n",min);
+return 0;
+}
