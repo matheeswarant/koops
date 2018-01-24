317e0adf9981b111c479c8ffa0c6228c51acadbc
matheeswarant <35597371+matheeswarant@users.noreply.github.com>
2018-01-24 16:48:04 +0530

    Create 31.c

---
+++ set4/31.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+char str[200],ch;
+int count=0,i=0;
+printf("/n enter the string \n");
+printf("/n count the number of character \n");
+scanf("%c",&ch);
+while(string [i]='\0')
+{
+if (str[i]==ch)
+count++;
+i++;
+}
+printf("/n count number of characters in the string \n");
+}
